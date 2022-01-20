145ce873150275d3c0156f3e398a9344825ae28d
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-01-20 19:23:40 +0530

    added lower banner image on home page

--- src/components/LowerBanner.js
+++ src/components/LowerBanner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LowerBanner = () => {
     return (
-        
+
         <div>
            <div className='relative bg-lowerbanner bg-center bg-cover h-screen  drop-shadow-lg'>
                 <div className='absolute top-20 w-full p-5 md:p-10'>
@@ -10,7 +10,7 @@ const LowerBanner = () => {
                     <h1 className='text-white text-5xl md:text-6xl lg:text-7xl font-Nunito font-bold'>about travelling ?</h1>
                 </div>
                 <div className='absolute bottom-20 p-5 md:p-10'>
-                    <button className='bg-red-400 text-white p-2 rounded-xl hover:bg-white hover:text-black text-md md:text-xl font-Nunito font-bold'>Ask Us</button>
+                    <button className='bg-red-400 text-white p-1 pl-3 pr-3 rounded-xl hover:bg-white hover:text-black text-md md:text-xl font-Nunito font-bold'>Ask Us</button>
                 </div>
                 <div className="absolute w-full h-28 bg-gradient-to-t from-black to-transparent bottom-0 z-20"/>
            </div>
