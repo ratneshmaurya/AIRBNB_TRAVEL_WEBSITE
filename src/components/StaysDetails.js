68856618b177b1f57c92a0928e29417a3103599f
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-01-21 01:49:07 +0530

    added search page JSON file

---
+++ src/components/StaysDetails.js
@@ -0,0 +1,62 @@
+//here i am storing all details of each stays places
+export const StaysDetails = [
+    {
+        id:"1",
+        image:"https://images.unsplash.com/photo-1594560913095-8cf34bab82ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
+        location:'Private room in center of mumbai',
+        title:"2 Bedroom Apartment for stay",
+        description:"3 guest. 2 bedrooms. 2 beds. 2 bathrooms ",
+        rating:4.5,
+        price:"2686",
+    },
+    {
+        id:"2",
+        image:"https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+        location:'Entire apartment in Indore',
+        title:"2 Bedroom Apartment in Vijay Nagar",
+        description:"8 guest. 2 bedrooms. 2 beds. 2 bathrooms ",
+        rating:4.6,
+        price:"2715",
+       
+    },
+    {
+        id:"3",
+        image:"https://images.unsplash.com/photo-1595576508898-0ad5c879a061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        location:'Entire apartment in Indore',
+        title:"The Peacock Sapphire- Home, terrace",
+        description:"10 guest. 3 bedrooms. 3 beds. 2 bathrooms ",
+        rating:4.7,
+        price:"3100",
+       
+    },
+    {
+        id:"4",
+        image:"https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        location:'Entire rental unit in Bengaluru',
+        title:"Ultra luxurious stay(minimum 3days)",
+        description:"4 guest. 1 bedrooms. 2 beds. 1 bathroom ",
+        rating:4.5,
+        price:"1539",
+        
+    },
+    {
+        id:"5",
+        image:"https://images.unsplash.com/photo-1592229505726-ca121723b8ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        location:'Entire serviced apartment in New Delhi',
+        title:"Isolated private studio apartment",
+        description:"2 guest. 1 bedrooms. 2 beds. 1 bathroom ",
+        rating:4.8,
+        price:"3199",
+       
+    },
+    {
+        id:"6",
+        image:"https://images.unsplash.com/photo-1505692433770-36f19f51681d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdGVsJTIwcm9vbXN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=1100&q=60",
+        location:'Entire cabin in Kausani',
+        title:"Everyday rinse and reload",
+        description:"4 guest. 1 bedrooms. 2 beds. 1 bathroom ",
+        rating:4.9,
+        price:"3600",
+        
+    },
+  ];
